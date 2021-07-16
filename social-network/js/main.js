0230b855aa2c1098d43739e7e4672dcde33117ff
ycazeau <ycazeau23@gmail.com>
2021-07-16 10:58:18 -0700

    Main Page - adding more Wall features

---
+++ social-network/js/main.js
@@ -0,0 +1,34 @@
+  // Slider class
+  class Headers extends HTMLElement{
+    connectedCallback(){
+      this.innerHTML = `
+      <!-- Header Section -->
+      <header>
+        <div class="container">
+        <img class="logo" src="img/logo.png" alt="">
+  
+        <form class="form-inline">
+          <label class="sr-only" for="inlineFormInputName2">Email</label>
+          <input type="email" class="form-control mb-2 mr-sm-2" id="inlineFormInputEmail" placeholder="Email">
+        
+          <label class="sr-only" for="inlineFormInputGrouppassword">Password</label>
+          <div class="input-group mb-2 mr-sm-2">          
+            <input type="password" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Password">
+          </div>      
+                
+          <button type="submit" class="btn btn-primary">LOGIN</button>
+  
+          <div class="form-check mb-2 mr-sm-2">
+            <input class="form-check-input" type="checkbox" id="inlineFormCheck">
+            <label class="form-check-label" for="inlineFormCheck">
+              Remember me
+            </label>
+          </div>
+        </form>
+        </div>
+      </header>
+      <!-- Header Section -->
+      `
+    }
+  }
+  customElements.define('my-header', Headers)
