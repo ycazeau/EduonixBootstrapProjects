b94bd3ffb7b395c9f633d38023ab4b687ab53d67
ycazeau <ycazeau23@gmail.com>
2021-07-17 08:42:00 -0700

    Creating Profile Page

--- social-network/js/main.js
+++ social-network/js/main.js
@@ -9,11 +9,11 @@
   
         <form class="form-inline">
           <label class="sr-only" for="inlineFormInputName2">Email</label>
-          <input type="email" class="form-control mb-2 mr-sm-2" id="inlineFormInputEmail" placeholder="Email">
+          <input type="email" class="form-control mb-2 mr-sm-2" id="inlineFormInputEmail" placeholder="Email" required>
         
           <label class="sr-only" for="inlineFormInputGrouppassword">Password</label>
           <div class="input-group mb-2 mr-sm-2">          
-            <input type="password" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Password">
+            <input type="password" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Password" required>
           </div>      
                 
           <button type="submit" class="btn btn-primary btn-login">LOGIN</button>
